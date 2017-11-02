f72e7ae776ce9dbdc4249ca66b33983cbe4c5ff1
Raisa ARC <raisarcl@gmail.com>
2017-11-03 00:38:04 +0100

    loucura, loucura... tamo fazendo mover pra direita

--- js/state.js
+++ js/state.js
@@ -28,6 +28,24 @@ game.state = {
 	]
 }
 
+function moveRight() {
+	// ver se tem quadradinho do mesmo valor que possa ser fundido + fundir
+	// ver se tem quadradinho pra mover + mover
+	for (var i = 3; i >= 0; i--) {
+		var row = game.state.grid[i];
+		var newRow = [0, 0, 0, 0];
+
+		//só vai até o penultimo tile, pq o ultimo tile (k=0) já foi comparado com todos
+		for (var j = 3; j > 0; j--) {			//comparar com outros tiles da linha
+			if(row[j] === 0)
+				continue;
+			for (k = j-1; k >= 0; k--) {
+				
+			}
+		}
+	}
+}
+
 function init() {
 	var htmlTiles = document.getElementsByClassName("tile"); //agrupar os elementos html vivos numa matriz chamada tiles
 															 //mais facil de acessar os tiles
@@ -41,10 +59,10 @@ function init() {
 function updateTiles() {
 	for (var i = 0; i < 4; i++) {
 		for (var j = 0; j < 4; j++) {
-			if (game.state.grid[i][j] === 0) {
+			if (game.state.grid[i][j] === 0) {						
 				continue;
 			}
-			game.tiles[i][j].innerHTML = game.state.grid[i][j];
+			game.tiles[i][j].innerHTML = game.state.grid[i][j];		// atualizar a "lousa" com a "ideia do professor"
 		}
 	}
 }
